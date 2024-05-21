821f745ad691339b593dc5ea639c89e0e07c7b98
Daniel Chenari <91308079+DanielC000@users.noreply.github.com>
2024-05-21 10:37:43 +0200

    init

---
+++ README.md
@@ -0,0 +1,70 @@
+# OpenMetadata Fiware Connector
+This repository is an custom [OpenMetadata](https://open-metadata.org/) Connector for the [FIWARE Context Broker](https://www.fiware.org/2020/12/11/fiware-context-broker-the-engine-for-future-energy-systems/).
+
+
+## Step 1 - Prepare the package installation
+We'll need to package the code so that it can be shipped to the ingestion container and used there. You can find a simple `setup.py` that builds the `connector` module.
+
+## Step 2 - Prepare the Ingestion Image
+
+If you want to use the connector from the UI, the `openmetadata-ingestion` image should be aware of your new package.
+
+We will be running the against the OpenMetadata version `1.2.0`, therefore, our Dockerfile looks like:
+
+```Dockerfile
+# Base image from the right version
+FROM openmetadata/ingestion:1.2.0
+
+# Let's use the same workdir as the ingestion image
+WORKDIR ingestion
+USER airflow
+
+# Install our custom connector
+# For a PROD image, this could be picking up the package from your private package index
+COPY connector connector
+COPY setup.py .
+RUN pip install requests
+RUN pip install --no-deps .
+```
+Build and use the new openmetadata-ingestion images in Docker compose:
+```yaml
+  ingestion:
+    container_name: openmetadata_ingestion
+    build:
+      context: ../
+      dockerfile: docker/Dockerfile
+```
+
+## Step 3 - Run OpenMetadata with the custom Ingestion image
+
+We have a `Makefile` prepared for you to run `make run`. This will get OpenMetadata up in Docker Compose using the custom Ingestion image.
+
+You may also just run:
+
+```cmd
+docker compose -f ./docker/docker-compose.yml up -d
+```
+
+## Step 4 - Configure the Connector
+
+In this guide we prepared a Database Connector. Thus, go to `Database Services > Add New Service > Custom` and set the `Source Python Class Name` as `connector.fiware_connector.FiwareConnector`.
+
+Note how we are specifying the full module name so that the Ingestion Framework can import the Source class.
+
+![demo.gif](images%2Fsetup_demo.gif)
+
+## OpenMetadata Fiware Connector
+
+To run the OpenMetadata Fiware Connector, the Python class will be `connector.fiware_connector.FiwareConnector` and we'll need to set the following Connection Options:
+- `broker_url`: The base URL to of the Fiware Conetxt Broker.
+- `database_name`: Any name you'd like - preferably no special characters.
+- `schema_name`: Any name you'd like - preferably no special characters.
+- `fiware_service`: Used to specify the Fiware service/tenant, when Muti Tenancy is enabled. Optional - if used given overrides `database_name`. (Read more about [Multi Tenacy](https://fiware-orion.readthedocs.io/en/master/orion-api.html#multi-tenancy).)
+- `fiware_service_path`: Used to specify the hierarchical scopes. Optional - if used given overrides `schema_name`. (Read more about [Hierarchical Scopes](https://fiware-orion.readthedocs.io/en/master/orion-api.html#service-path).) This option is currently untested. Issues with `/` used in service-paths could occur.
+
+## Contributing
+
+Everyone is invited to get involved and contribute to the project.
+
+Simply create a [fork and pull request](https://docs.github.com/en/get-started/quickstart/contributing-to-projects) for code contributions or
+feel free to [open an issue](https://github.com/msgis/openmetadata-fiware-connector/issues) for any other contributions or issues.
---
+++ connector/fiware_connector.py
@@ -0,0 +1,200 @@
+import requests
+from pathlib import Path
+from typing import Iterable, Optional, List, Any
+from re import sub
+
+from metadata.ingestion.api.common import Entity
+from metadata.ingestion.api.models import Either
+from metadata.ingestion.api.steps import Source, InvalidSourceException
+from metadata.generated.schema.entity.services.connections.metadata.openMetadataConnection import (
+    OpenMetadataConnection,
+)
+from metadata.generated.schema.entity.services.connections.database.customDatabaseConnection import (
+    CustomDatabaseConnection,
+)
+from metadata.generated.schema.entity.data.database import Database
+from metadata.generated.schema.entity.data.databaseSchema import DatabaseSchema
+from metadata.generated.schema.api.data.createDatabaseSchema import (
+    CreateDatabaseSchemaRequest,
+)
+from metadata.generated.schema.api.data.createDatabase import CreateDatabaseRequest
+from metadata.generated.schema.entity.services.databaseService import (
+    DatabaseService,
+)
+from metadata.generated.schema.entity.data.table import (
+    Column,
+)
+from metadata.generated.schema.metadataIngestion.workflow import (
+    Source as WorkflowSource,
+)
+from metadata.generated.schema.api.data.createTable import CreateTableRequest
+from metadata.ingestion.ometa.ometa_api import OpenMetadata
+from metadata.utils.logger import ingestion_logger
+
+logger = ingestion_logger()
+
+class InvalidFiwareConnectorException(Exception):
+    """
+    Sample data is not valid to be ingested
+    """
+
+class FiwareConnector(Source):
+    """
+    Custom connector to ingest Database metadata from various formats.
+    """
+    def __init__(self, config: WorkflowSource, metadata: OpenMetadata):
+        self.config = config
+        self.metadata = metadata
+
+        self.service_connection = config.serviceConnection.__root__.config
+        self.broker_url: str = self.service_connection.connectionOptions.__root__.get("broker_url")
+        self.database_name: str = self.service_connection.connectionOptions.__root__.get("database_name")
+        self.schema_name: str = self.service_connection.connectionOptions.__root__.get("schema_name")
+        self.fiware_service: str = self.service_connection.connectionOptions.__root__.get("fiware_service")
+        self.fiware_service_path: str = self.service_connection.connectionOptions.__root__.get("fiware_service_path")
+
+        if not self.broker_url:
+            raise InvalidFiwareConnectorException("Missing required connection option 'broker_url' in service connection.")
+        
+        if not self.database_name and not self.fiware_service:
+            raise InvalidFiwareConnectorException("Missing required connection option 'database_name' or 'fiware_service' in service connection.")
+        
+        if not self.schema_name and not self.fiware_service_path:
+            raise InvalidFiwareConnectorException("Missing required connector option 'schema_name' or 'fiware_service_path' in service connection.")
+    
+    
+        self.data: Optional[List[Any]] = None
+        super().__init__()
+
+    @classmethod
+    def create(
+        cls, config_dict: dict, metadata_config: OpenMetadataConnection
+    ) -> "FiwareConnector":
+        config: WorkflowSource = WorkflowSource.parse_obj(config_dict)
+        connection: CustomDatabaseConnection = config.serviceConnection.__root__.config
+        if not isinstance(connection, CustomDatabaseConnection):
+            raise InvalidSourceException(
+                f"Expected CustomDatabaseConnection, but got {connection}"
+            )
+        return cls(config, metadata_config)
+
+    def prepare(self):
+        headers = {
+            'Accept': 'application/json',
+        }
+        
+        if self.fiware_service:
+            headers = headers + {'Fiware-Service': self.fiware_service}
+        if self.fiware_service_path:
+            headers = headers + {'Fiware-ServicePath': self.fiware_service_path}
+        self.data = self.query_context_broker(self.broker_url, headers)
+
+    def query_context_broker(self, base_url, headers):
+        data = []
+        query_url = base_url + "/v2/types"
+        try:
+            response = requests.get(query_url, headers=headers)
+            data = response.json()
+            logger.info(f"DATA: {data}")
+        except Exception as e:
+            logger.error(f"Error querying context broker on {base_url}: {e}")
+            raise InvalidFiwareConnectorException(f"Error querying context broker on {base_url}: {e}")
+    
+        return data
+
+    def yield_create_request_database_service(self):
+        yield Either(
+            right=self.metadata.get_create_service_from_source(
+                entity=DatabaseService, config=self.config
+            )
+        )
+
+    def yield_db_name(self):
+        # Pick up the service we just created (if not UI)
+        service_entity: DatabaseService = self.metadata.get_by_name(
+                entity=DatabaseService, fqn=self.config.serviceName
+            )
+        
+        if not self.fiware_service:
+            name = self.database_name
+        else:
+            name = self.fiware_service
+
+        yield Either(
+            right=CreateDatabaseRequest(
+                name=name,
+                service=service_entity.fullyQualifiedName,
+            )
+        )
+
+    def yield_schema(self):
+        # Pick up the service we just created (if not UI)
+        database_entity: Database = self.metadata.get_by_name(
+            entity=Database, fqn=f"{self.config.serviceName}.{self.database_name}"
+        )
+
+        if not self.fiware_service_path:
+            name = self.schema_name
+        else:
+            name = self.fiware_service_path
+
+        yield Either(
+            right=CreateDatabaseSchemaRequest(
+                name=name,
+                database=database_entity.fullyQualifiedName,
+            )
+        )
+
+    def yield_data(self):
+        """
+        Iterate over the data list to create tables
+        """
+        if not self.fiware_service_path:
+            schema = self.schema_name
+        else:
+            schema = self.fiware_service_path
+        
+        database_schema: DatabaseSchema = self.metadata.get_by_name(
+            entity=DatabaseSchema,
+            fqn=f"{self.config.serviceName}.{self.database_name}.{schema}",
+        )
+
+        for type in self.data:
+            yield Either(
+                right=CreateTableRequest(
+                    name=type['type'],
+                    databaseSchema=database_schema.fullyQualifiedName,
+                    columns=[
+                        Column(
+                            name=key,
+                            dataType=self.map_datatypes(key, type['attrs']),
+                            description=type['attrs'][key]['types'][0],
+                        )
+                        for key in type['attrs'].keys()
+                    ],
+                )
+            )
+         
+    def map_datatypes(self, key, datatype):
+        if key == 'location':
+            return 'JSON'
+        elif 'int' in datatype:
+            return 'INT'
+        elif 'float' in datatype:
+            return 'FLOAT'
+        elif 'str' in datatype:
+            return 'STRING'
+        else:
+            return 'NULL'
+        
+    def _iter(self) -> Iterable[Entity]:
+        yield from self.yield_create_request_database_service()
+        yield from self.yield_db_name()
+        yield from self.yield_schema()
+        yield from self.yield_data()
+
+    def test_connection(self) -> None:
+        pass
+
+    def close(self):
+        pass
